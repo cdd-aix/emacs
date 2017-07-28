8adea8b55e1ec9393b07c4b6fbf9a2169a2e9de9
Chris Dukes <chris.dukes.aix@gmail.com>
2017-07-28 02:28:30 -0400

    Captured elpa state.

---
+++ elpa/ghub-20170725.1254/ghub-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "ghub" "20170725.1254" "minuscule client for the Github API" '((emacs "25")) :commit "d63432c86ed8007be8fd85125e1eb432f122e8a0" :url "https://github.com/magit/ghub" :keywords '("tools"))
