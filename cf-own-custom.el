306d92e3b249499e401977f73103c83cfc9cfae6
Chris Dukes <chris.dukes.aix@gmail.com>
2017-07-28 02:28:00 -0400

    And finalized

--- cf-own-custom.el
+++ cf-own-custom.el
@@ -1,3 +1,5 @@
+;;; package --- Summary
+;;; Code:
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -8,10 +10,13 @@
     ("3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" default)))
  '(package-selected-packages
    (quote
-    (smart-mode-line elpy default-text-scale neotree smex rjsx-mode expand-region bbdb yaml-mode xml-rpc use-package rainbow-mode nginx-mode markdown-mode magithub json-reformat json-mode jira jinja2-mode jekyll-modes hyde helm-projectile groovy-mode flycheck-pos-tip flycheck-color-mode-line flycheck-checkbashisms dockerfile-mode color-theme coffee-mode ansible-vault ansible-doc ansible))))
+    (dtrt-indent ws-butler which-key smart-mode-line elpy default-text-scale neotree smex rjsx-mode expand-region bbdb yaml-mode xml-rpc use-package rainbow-mode nginx-mode markdown-mode magithub json-reformat json-mode jira jinja2-mode jekyll-modes hyde helm-projectile groovy-mode flycheck-pos-tip flycheck-color-mode-line flycheck-checkbashisms dockerfile-mode color-theme coffee-mode ansible-vault ansible-doc ansible))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+;;; Commentary:
+(provide 'cf-own-custom)
+;;; cf-own-custom.el ends here
