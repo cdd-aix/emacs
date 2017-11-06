ce1296ab825e696ba4ee5514c3cd07c34e0c23af
Chris Dukes <chris.dukes.aix@gmail.com>
2017-11-06 17:52:04 -0500

    Fixed typo

--- rebuild.bash
+++ rebuild.bash
@@ -1,4 +1,4 @@
 #!/bin/bash -e
-[ -r cf-own-custom.el ] || cp cf-own-customer.el.default cf-own-custom.el
+[ -r cf-own-custom.el ] || cp cf-own-custom.el.default cf-own-custom.el
 rm -f init.elc
 emacs -Q --batch -l init.el -f batch-byte-compile init.el
