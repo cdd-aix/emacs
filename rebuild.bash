c8df5ad6b83aebb66108c9b0a48033e94937d73b
Chris Dukes <chris.dukes.aix@gmail.com>
2018-10-15 18:36:58 -0400

    Add better userdir recompile logic

--- rebuild.bash
+++ rebuild.bash
@@ -1,4 +1,16 @@
-#!/bin/bash -e
+#!/bin/bash -eux
 #[ -r cf-own-custom.el ] || cp cf-own-custom.el.default cf-own-custom.el
-rm -f init.elc
-emacs -Q --batch -l init.el -f batch-byte-compile init.el
+zero=$(readlink -f "$0")
+zerodir="${zero%/*}"
+ebatch() {
+    emacs -Q --batch -l "$zerodir/init.el" "$@"
+}
+main() {
+    local userdir
+    rm -f init.elc
+    ebatch -f batch-byte-compile "$zerodir/init.el"
+    userdir=$(ebatch --eval '(princ package-user-dir)' 2>/dev/null)
+    ebatch -f batch-byte-recompile-directory "$userdir"
+    ebatch -f batch-byte-recompile init.el
+}
+main "$@"
