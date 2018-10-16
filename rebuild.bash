c99c1079059f5a8785b44943956268dc2d1bdc07
Chris Dukes <chris.dukes.aix@gmail.com>
2018-10-15 20:05:38 -0400

    Fix recompile to actually recompile.

--- rebuild.bash
+++ rebuild.bash
@@ -1,5 +1,6 @@
 #!/bin/bash -eux
 #[ -r cf-own-custom.el ] || cp cf-own-custom.el.default cf-own-custom.el
+export LANG=en_US.UTF-8
 zero=$(readlink -f "$0")
 zerodir="${zero%/*}"
 ebatch() {
@@ -10,7 +11,7 @@ main() {
     rm -f init.elc
     ebatch -f batch-byte-compile "$zerodir/init.el"
     userdir=$(ebatch --eval '(princ package-user-dir)' 2>/dev/null)
-    ebatch -f batch-byte-recompile-directory "$userdir"
+    ebatch --eval "(byte-recompile-directory \"$userdir\" 0 t)"
     ebatch -f batch-byte-compile init.el
 }
 main "$@"
