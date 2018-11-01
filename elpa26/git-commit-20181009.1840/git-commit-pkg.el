0c357136a644202e17405a8554a70f90cde457c5
Chris Dukes <chris.dukes.aix@gmail.com>
2018-11-01 13:33:46 -0400

    Discard major version elpas.

--- elpa26/git-commit-20181009.1840/git-commit-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "git-commit" "20181009.1840" "Edit Git commit messages" '((emacs "25.1") (dash "20180413") (with-editor "20180414")) :commit "2caa0390ef2eebd0c8fa938c577abcbf39509476" :keywords '("git" "tools" "vc") :maintainer '("Jonas Bernoulli" . "jonas@bernoul.li") :url "https://github.com/magit/magit")
