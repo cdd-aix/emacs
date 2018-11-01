0a9cde12b75ac762100c1e8dcdc81d025651b712
Chris Dukes <chris.dukes.aix@gmail.com>
2018-11-01 13:33:13 -0400

    Fix, don't track major version.  Not Needed.

---
+++ elpa/epm-20161027.734/epm-autoloads.el
@@ -0,0 +1,16 @@
+;;; epm-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil nil ("epm-pkg.el" "epm.el") (23515 14389 808970
+;;;;;;  938000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; epm-autoloads.el ends here
---
+++ elpa/f-20180106.922/f-autoloads.el
@@ -0,0 +1,15 @@
+;;; f-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil nil ("f.el") (23515 14440 820965 388000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; f-autoloads.el ends here
