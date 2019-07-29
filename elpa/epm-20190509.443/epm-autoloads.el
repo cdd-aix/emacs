361750dda53058d1865f3bcca108d4069ed8103e
Chris Dukes <chris.dukes.aix@gmail.com>
2019-07-29 11:21:22 -0400

    20190729 update

---
+++ elpa/epm-20190509.443/epm-autoloads.el
@@ -0,0 +1,16 @@
+;;; epm-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil nil ("epm-pkg.el" "epm.el") (23870 62219 805150
+;;;;;;  798000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; epm-autoloads.el ends here
