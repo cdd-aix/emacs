354f0258845d14862fc04028809dd8a8576a7330
Chris Dukes <pakraticus@gmail.com>
2019-10-17 13:20:09 -0400

    update packages 20191017

---
+++ elpa/yasnippet-snippets-20191010.1106/yasnippet-snippets-autoloads.el
@@ -0,0 +1,29 @@
+;;; yasnippet-snippets-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil "yasnippet-snippets" "yasnippet-snippets.el"
+;;;;;;  (23976 41505 798869 520000))
+;;; Generated autoloads from yasnippet-snippets.el
+
+(autoload 'yasnippet-snippets-initialize "yasnippet-snippets" "\
+Load the `yasnippet-snippets' snippets directory.
+
+\(fn)" nil nil)
+
+(eval-after-load 'yasnippet '(yasnippet-snippets-initialize))
+
+;;;***
+
+;;;### (autoloads nil nil ("yasnippet-snippets-pkg.el") (23976 41513
+;;;;;;  302869 474000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; yasnippet-snippets-autoloads.el ends here
