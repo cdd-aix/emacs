0f808e6950a16c768522395be9cc74a0908dd027
Chris Dukes <chris.dukes.aix@gmail.com>
2019-10-17 15:16:55 -0400

    Updated build script to fix weird package missing problem.

--- rebuild.bash
+++ rebuild.bash
@@ -7,9 +7,21 @@ ebatch() {
     emacs -Q --batch -l "$zerodir/init.el" "$@"
 }
 main() {
-    local results
-    rm -f "$zerodir/init.elc"
-    results=$(time ebatch --eval "(byte-recompile-directory package-user-dir 0 t)" -f batch-byte-compile "$zerodir/init.el" 2>compile.log)
+    local packageuserdir bytecompile
+    # Side effect, installs missing packages
+    rm -vf "$zerodir/init.elc"
+    echo See fetch.log if packages are missing
+    packageuserdir=$(ebatch --eval '(princ package-user-dir)' 2> fetch.log)
+    echo package-user-dir "$packageuserdir"
+    echo See compile.log if other weirdness
+    bytecompile=$(ebatch --eval '(byte-recompile-directory package-user-dir 0 t)' -f batch-byte-compile "$zerodir/init.el" 2>compile.log)
+    echo Byte Compile Results "$bytecompile"
+    ebatch -f batch-byte-compile "$zerodir/init.el"
+    # rm -f "$zerodir/init.elc"
+    # if ! [ -d "$zerodir/elpa" ]; then
+    # 	ebatch -f
+    # results=$(time ebatch --eval "(byte-recompile-directory package-user-dir 0 t)" -f batch-byte-compile "$zerodir/init.el" 2>compile.log)
+    # echo "$results"
     ls -ld "$zerodir/init.elc"
 }
 main "$@"
