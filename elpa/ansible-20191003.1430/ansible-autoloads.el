7b9cfe9ac05926c9f3900c6596e46319caee9d81
Chris Dukes <chris.dukes.aix@gmail.com>
2019-10-17 15:16:23 -0400

    Refreshed packages.

---
+++ elpa/ansible-20191003.1430/ansible-autoloads.el
@@ -0,0 +1,35 @@
+;;; ansible-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil "ansible" "ansible.el" (23976 47688 410831
+;;;;;;  755000))
+;;; Generated autoloads from ansible.el
+
+(defvar ansible-key-map (make-sparse-keymap) "\
+Keymap for Ansible.")
+
+(autoload 'ansible "ansible" "\
+Ansible minor mode.
+
+\(fn &optional ARG)" t nil)
+
+(autoload 'ansible-dict-initialize "ansible" "\
+Initialize Ansible auto-complete.
+
+\(fn)" nil nil)
+
+;;;***
+
+;;;### (autoloads nil nil ("ansible-pkg.el") (23976 47688 382831
+;;;;;;  755000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; ansible-autoloads.el ends here
