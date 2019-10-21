1c9367ae052af4119696509c3c2701d9f7843648
Chris Dukes <pakraticus@gmail.com>
2019-10-21 16:24:45 -0400

    Clean slate for rewrite with old pieces for reference.

--- elpa/dash-20190920.1035/dash-autoloads.el
+++
@@ -1,15 +0,0 @@
-;;; dash-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
-
-;;;### (autoloads nil nil ("dash.el") (23981 511 300942 52000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; dash-autoloads.el ends here
